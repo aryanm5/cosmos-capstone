b143a155b593e112d315beb933ed6ceb0b878a70
aryanm5 <aryanm.email@gmail.com>
2022-07-27 13:32:33 -0700

    added more comments to model code

--- aryan.R
+++ aryan.R
@@ -5,29 +5,15 @@ library(tidyverse)
 data <- readr::read_csv("alzheimer_data.csv")
 attach(data)
 
-# Add bin_diag column
-
-# data <- data %>%
-#   mutate(bin_diag = as.integer(diagnosis > 0))
-# attach(data)
-# 
-# write.table(data, "alzheimer_data.csv", quote=FALSE, eol="\n\n", row.names=FALSE, sep=",")
-
-
-# GLM TESTING
+# MODEL
 
 library(glmnet)
 
-# data <- data %>%
-#  filter(games >= 0 & games < 4)
+test_range <- 540 # Length of testing dataset (540 = 20% of training dataset)
+test_data <- head(data, test_range) # Get first 540 rows
+data <- tail(data, -test_range) # Delete first 540 rows from training data
 
-#glm(bin_diag ~ games, "binomial", data)
-#?glm
-
-test_range <- 540
-test_data <- head(data, test_range)
-data <- tail(data, -test_range)
-
+# xfactors for categorical variables, x for numerical
 xfactors <- model.matrix(bin_diag ~ as.factor(games) + as.factor(taxes) + as.factor(motsev) + as.factor(hallsev) + as.factor(travel))[, -1]
 x        <- as.matrix(data.frame(traila, trailb, csfvol, xfactors))
 
@@ -38,29 +24,29 @@ glmmod <- glmnet(x, y=as.factor(bin_diag), alpha=1, family="binomial")
 # Plot variable coefficients vs. shrinkage parameter lambda.
 plot(glmmod, xvar="lambda")
 
-coef(glmmod)[,10]
+coef(glmmod)[,10] # Get coefficients (beta variables)
 
 cv.glmmod <- cv.glmnet(x, y=bin_diag, alpha=1)
 plot(cv.glmmod)
 
-(best.lambda <- cv.glmmod$lambda.min)
+#model <- glm(bin_diag ~ x) # this is the model with no lasso
 
+(best.lambda <- cv.glmmod$lambda.min) # Best lambda value? Not sure
 
+# Inputs to predict from
 test_xfactors <- model.matrix(test_data$bin_diag ~ as.factor(test_data$games) + as.factor(test_data$taxes) + as.factor(test_data$motsev) + as.factor(test_data$hallsev) + as.factor(test_data$travel))[, -1]
 test_x        <- as.matrix(data.frame(test_data$traila, test_data$trailb, test_data$csfvol, test_xfactors))
 
+actual <- test_data$bin_diag # Actual values to compare to prediction
+pred <- predict(cv.glmmod, newx = test_x) # Run prediction
 
+cutoff <- 0.5 # Prediction is > or < than 0.5.
+pred <- replace(pred, pred < cutoff, 0) # Replace predictions < 0.5 with 0
+pred <- replace(pred, pred >= cutoff, 1) # Replace predictions >= 0.5 with 1
 
-actual <- test_data$bin_diag[1:test_range]
-pred <- predict(cv.glmmod, newx = test_x)
+plot(actual, lwd=1, type="o", col="blue", ylab="Actual vs. Predicted") # Plot actual values
 
-cutoff <- 0.5
-pred <- replace(pred, pred<cutoff, 0)
-pred <- replace(pred, pred>cutoff, 1)
-
-plot(actual, lwd=1, type="o", col="blue", ylab="Actual vs. Predicted")
-
-lines(pred, lwd=2, type="o", col="red")
+lines(pred, lwd=2, type="o", col="red") # Plot predicted values
 
 # Calculate accuracy
 count <- 0
