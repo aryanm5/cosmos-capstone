a3f4cd3ac020ae8849312b3fb8c0c586a5841b15
Michael Murphy <michaelmurphy@dhcp-v035-099.mobile.uci.edu>
2022-07-27 11:47:42 -0700

    Created more visualizations and a glm model for some variables and tested hypotheses from some research papers

--- michael.R
+++ michael.R
@@ -3,6 +3,7 @@ library(readr)
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
+library(glmnet)
 
 alzheimer_data = read_csv("alzheimer_data.csv")
 alz = alzheimer_data
@@ -43,3 +44,38 @@ alz_motsev_non0 = filter(alz, motsev > 0 & motsev < 4)
 motsev_perc = nrow(filter(alz_motsev_non0, diagnosis > 0)) / nrow(alz_motsev_non0)
 
 nrow(alz_motsev_non0)
+
+#csfvol plot + test
+alz %>% ggplot(aes(y = csfvol, color = as.factor(diagnosis))) + geom_boxplot()
+
+t.test(csfvol, diagnosis)
+
+#anxsev plot
+alz %>% ggplot(aes(x = as.factor(anxsev), fill = as.factor(diagnosis))) + geom_bar()
+
+#TESTING HALLUCINATION ARTICLE HYPOTHESES
+#naccicv vs hallsev plot
+alz %>% 
+  ggplot(aes(x = naccicv, y = hallsev, color = as.factor(diagnosis))) + geom_point()
+
+alz %>% ggplot(aes(x = naccicv, fill = as.factor(hallsev))) + geom_histogram()
+
+alz %>% ggplot(aes(y = naccicv, color = as.factor(hallsev))) + geom_boxplot()
+
+alz %>% ggplot(aes(y = csfvol, color = as.factor(hallsev))) + geom_boxplot()
+
+#is hallsev actually a better predictor than motsev and appsev? (answer = YES)
+
+alz_hallsev_non0 = filter(alz, hallsev > 0 & hallsev < 4)
+hallsev_perc = nrow(filter(alz_hallsev_non0, diagnosis > 0)) / nrow(alz_hallsev_non0)
+
+alz_motsev_non0 = filter(alz, motsev > 0 & motsev < 4)
+motsev_perc = nrow(filter(alz_motsev_non0, diagnosis > 0)) / nrow(alz_motsev_non0)
+
+alz_appsev_non0 = filter(alz, appsev > 0 & appsev < 4)
+appsev_perc = nrow(filter(alz_appsev_non0, diagnosis > 0)) / nrow(alz_appsev_non0)
+
+#modeling different things
+diag_logistic <- glm(bin_diag~games+traila+trailb+female+anxsev+weight,family=binomial)
+plot(diag_logistic)
+
